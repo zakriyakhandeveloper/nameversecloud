d5f957c0b7146b3e412f956712be4c0b8ae48b75
zakriya-khan-developer <zk145212128@gmail.com>
2026-07-28 10:10:48 +0500

    'nchanges'

--- open-next.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-
-export default defineCloudflareConfig({
-  override: {
-    experimental: {
-      runtime: 'edge',
-    },
-  },
-});
--- scripts/build-sitemap.js
+++
@@ -1,9 +0,0 @@
-import { writeSitemapFiles } from '../src/lib/seo/sitemap-data.mjs';
-
-try {
-  const result = await writeSitemapFiles();
-  console.log(JSON.stringify(result, null, 2));
-} catch (error) {
-  console.error(error);
-  process.exit(1);
-}
